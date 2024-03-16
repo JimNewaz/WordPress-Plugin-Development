6fc59b227a845b76d2c2e71c8efeff82dbf11b56
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2024-03-16 14:39:02 +0600

    Basic security

---
+++ wp-content/plugins/ring-design/ring-design.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+**
+    Plugin Name: Custom Ring Design 
+    Description: This is a plugin where you can customize your ring. 
+    Version: 1.0.0 
+    Text Domain: options-plugin
+**
+*/
+
+if(!defined('ABSPATH')){
+    die('You cannot be here');
+}
+
