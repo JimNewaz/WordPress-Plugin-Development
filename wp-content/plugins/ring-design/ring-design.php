ead6b3f53448ee7b64e48e46cc25a9ea2fb9fc2c
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2024-03-16 14:59:53 +0600

    Carbon Fileds library

--- wp-content/plugins/ring-design/ring-design.php
+++ wp-content/plugins/ring-design/ring-design.php
@@ -5,6 +5,8 @@
     Description: This is a plugin where you can customize your ring. 
     Version: 1.0.0 
     Text Domain: options-plugin
+    Author: Sayed Nur E Newaz
+    Domain Path: /languages
 **
 */
 
@@ -12,3 +14,14 @@ if(!defined('ABSPATH')){
     die('You cannot be here');
 }
 
+if(!class_exists('CustomRingDesign')){
+    class CustomRingDesign{
+        public function __construct()
+        {
+            define('MY_PLUGIN_PATH', plugin_dir_path( __FILE__ )); 
+
+            require_once(MY_PLUGIN_PATH. '/vendor/autoload.php');
+        }
+    }
+new CustomRingDesign;
+}
