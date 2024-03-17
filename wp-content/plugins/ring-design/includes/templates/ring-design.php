9bbdeef0486dc5d483b78ca4de75d1596f609121
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2024-03-17 12:00:38 +0600

    add template for view

--- wp-content/plugins/ring-design/includes/templates/ring-design.php
+++ wp-content/plugins/ring-design/includes/templates/ring-design.php
@@ -1,1 +1,13 @@
-wadawdw
+<form>
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" value="Mike"><br><br>
+
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" value="Walker"><br><br>
+
+    <label for="phone">Phone</label><br>
+    <input type="number" id="phone" value="12346789"><br><br>
+
+    <input type="submit" value="Submit">
+</form>
+
