6a5e1d3327abf6865a3404c3f46a31dfa8f14e94
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2024-03-17 12:22:09 +0600

    set rest endpoint

--- wp-content/plugins/ring-design/includes/ring-design.php
+++ wp-content/plugins/ring-design/includes/ring-design.php
@@ -1,7 +1,23 @@
-<?php 
+<?php
 
-add_shortcode('ring_design', 'show_ring_design_form'); 
+add_shortcode('ring_design', 'show_ring_design_form');
 
-function show_ring_design_form(){
+add_action('rest_api_init', 'create_rest_endpoint');
+
+function show_ring_design_form()
+{
     include_once MY_PLUGIN_PATH . '/includes/templates/ring-design.php';
+}
+
+function create_rest_endpoint()
+{
+    register_rest_route('v1/ring-design/', 'submit', array(
+        'method' => 'POST',
+        'callback' => 'handle_enquiry',
+    ));
+}
+
+function handle_enquiry()
+{
+    echo 'Hello';
 }
