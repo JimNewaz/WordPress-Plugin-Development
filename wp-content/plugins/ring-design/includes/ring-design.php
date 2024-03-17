05c457b9681ad422d6efbb9bed6db3a79905bdeb
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2024-03-17 13:29:48 +0600

    ajax point to rest endpoint

--- wp-content/plugins/ring-design/includes/ring-design.php
+++ wp-content/plugins/ring-design/includes/ring-design.php
@@ -4,20 +4,30 @@ add_shortcode('ring_design', 'show_ring_design_form');
 
 add_action('rest_api_init', 'create_rest_endpoint');
 
+add_action('wp_enqueue_scripts', 'enqueue_scripts');
+
 function show_ring_design_form()
 {
+    ob_start();
     include_once MY_PLUGIN_PATH . '/includes/templates/ring-design.php';
+    return ob_get_clean();
 }
 
 function create_rest_endpoint()
 {
-    register_rest_route('v1/ring-design/', 'submit', array(
-        'method' => 'POST',
+    register_rest_route('v1/ring-design', 'submit', array(
+        'methods' => 'POST',
         'callback' => 'handle_enquiry',
     ));
+}
+
+function enqueue_scripts()
+{
+    wp_enqueue_script('jquery');
 }
 
 function handle_enquiry()
 {
     echo 'Hello';
 }
+
--- wp-content/plugins/ring-design/includes/templates/ring-design.php
+++ wp-content/plugins/ring-design/includes/templates/ring-design.php
@@ -1,13 +1,32 @@
-<form>
+<form id="enquiry_form">
+    <?php wp_nonce_field('wp_rest');?>
+
     <label for="fname">First name:</label><br>
-    <input type="text" id="fname" value="Mike"><br><br>
+    <input type="text" id="fname" name="fname" value="Mike"><br><br>
 
     <label for="lname">Last name:</label><br>
-    <input type="text" id="lname" value="Walker"><br><br>
+    <input type="text" id="lname" name="lname" value="Walker"><br><br>
 
     <label for="phone">Phone</label><br>
-    <input type="number" id="phone" value="12346789"><br><br>
+    <input type="number" id="phone" name="phone" value="12346789"><br><br>
 
-    <input type="submit" value="Submit">
+    <button type ="submit">Submit</button>
 </form>
+
+<script>
+    jQuery(document).ready(function($){
+        $('#enquiry_form').submit(function(event){
+
+            event.preventDefault();
+            var form = $(this);            
+            
+            $.ajax({
+                type: 'POST',                
+                url: "<?php echo get_rest_url(null, 'v1/ring-design/submit'); ?>",
+                data: form.serialize()
+            });
+            
+        });
+    });
+</script>
 
