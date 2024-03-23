d8e46c7c3e24215dbdf4d1663667bbba82992499
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2024-03-23 21:32:46 +0600

    Bug Fix

--- wp-content/plugins/ring-design/includes/templates/ring-design.php
+++ wp-content/plugins/ring-design/includes/templates/ring-design.php
@@ -471,7 +471,7 @@
                                     required>
                                     <option value="zar">ZAR</option>
                                     <option value="usd">USD</option>
-                                    <option value="gbp">USD</option>
+                                    <option value="gbp">GBP</option>
                                     <option value="eur">EUR</option>
                                     <option value="aud">AUD</option>
                                 </select>
@@ -488,7 +488,7 @@
                                     <option value="50,000 - 100,000">50,000 - 100,000</option>
                                     <option value="100,000 - 200,000">100,000 - 200,000</option>
                                     <option value="200,000+">200,000+</option>
-                                    <option value="advice">Need Advice</option>
+                                    <option value="Need Advice">Need Advice</option>
                                 </select>
                             </div>
 
